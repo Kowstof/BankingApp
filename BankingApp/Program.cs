b8f38b91661573dd4012da5baf69f115a162542d
Krystof <krystofpavlis@gmail.com>
2022-08-28 00:34:56 +1000

    added missing files

---
+++ BankingApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace BankingApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList accounts = new ArrayList();
+            
+            Console.WriteLine("Hello World!");
+            Login();
+            Console.ReadKey();
+        }
+
+        private static void Login()
+        {
+            Console.WriteLine("Welcome to Krystof Bank!");
+            Console.WriteLine("Customer ID: ");
+            Console.WriteLine("Password: ");
+        }
+    }
+}
